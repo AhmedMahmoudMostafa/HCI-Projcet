b701553c72bae2b7fe07bcf76626fb6f0c92b40b
AhmedDiaaeldin <101406020+AhmedDiaaeldin@users.noreply.github.com>
2023-12-26 03:03:30 +0200

    Add files via upload

---
+++ Frontend/src/components/GeneralScreens/Home.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
+import { useLocation } from 'react-router-dom';
+import io from 'socket.io-client';
+import SkeletonStory from '../Skeletons/SkeletonStory';
+import CardStory from '../StoryScreens/CardStory';
+import NoStories from '../StoryScreens/NoStories';
+import Pagination from './Pagination';
+import '../../Css/Home.css';
+
+const Home = () => {
+    const search = useLocation().search;
+    const searchKey = new URLSearchParams(search).get('search');
+    const [stories, setStories] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [pages, setPages] = useState(1);
+    const [page, setPage] = useState(1);
+    const [displayCategories, setDisplayCategories] = useState([]);
+
+    useEffect(() => {
+        const socket = io('http://localhost:3000');
+
+        socket.on('categoryUpdate', (data) => {
+            console.log('Received category update:', data);  // For debugging
+            const categoriesToShow = Object.keys(data).filter(category => data[category]);
+            setDisplayCategories(categoriesToShow);
+        });
+
+        return () => socket.disconnect();
+    }, []);
+
+    useEffect(() => {
+        const fetchStories = async () => {
+            setLoading(true);
+            try {
+                const response = await axios.get(`/story/getAllStories?search=${searchKey || ''}&page=${page}`);
+                let fetchedStories = response.data.data;
+
+                // Apply filter only if there are categories to show
+                if (displayCategories.length > 0) {
+                    fetchedStories = fetchedStories.filter(story => displayCategories.includes(story.category));
+                }
+
+                setStories(fetchedStories);
+                setPages(response.data.pages);
+            } catch (error) {
+                console.error('Error fetching stories:', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchStories();
+    }, [search, page, displayCategories]);
+
+    useEffect(() => {
+        setPage(1);
+    }, [searchKey]);
+
+    return (
+        <div className="Inclusive-home-page">
+            {loading ? (
+                <div className="skeleton_emp">
+                    {[...Array(6)].map(() => <SkeletonStory key={uuidv4()} />)}
+                </div>
+            ) : (
+                <div>
+                    <div className="story-card-wrapper">
+                        {stories.length > 0 ? (
+                            stories.map((story) => <CardStory key={story._id} story={story} />)
+                        ) : (
+                            <NoStories />
+                        )}
+                    </div>
+                    <Pagination page={page} pages={pages} changePage={setPage} />
+                </div>
+            )}
+            <br />
+        </div>
+    );
+};
+
+export default Home;
